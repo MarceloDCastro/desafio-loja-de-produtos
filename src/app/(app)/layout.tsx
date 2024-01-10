3f9fff0e65c382d45feb4fdd390f034b8402d46c
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2024-01-10 16:12:41 -0300

    fix: header on mobile

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -12,16 +12,16 @@ export default async function AppLayout({ children }: { children: ReactNode }) {
 
   return (
     <div>
-      <header className="h-16 w-full border-b flex items-center justify-between px-5">
+      <header className="h-16 w-full border-b flex items-center justify-between px-2 sm:px-5">
         <div className="flex items-center">
           <Link href="/">
-            <div className="flex gap-2 items-center mr-16">
+            <div className="flex gap-2 items-center mr-4 sm:mr-16">
               <ShoppingBag />
               <h1 className="text-xl font-bold tracking-tight">Loja</h1>
             </div>
           </Link>
 
-          <nav className="flex gap-6 text-sm">
+          <nav className="flex gap-4 sm:gap-6 text-sm items-center">
             <NavLink href="/">Produtos</NavLink>
             {session && (
               <NavLink href="/cadastrar-produto">Cadastrar produto</NavLink>
@@ -29,7 +29,7 @@ export default async function AppLayout({ children }: { children: ReactNode }) {
           </nav>
         </div>
 
-        <div className="flex gap-6">
+        <div className="flex gap-4 sm:gap-6">
           <CartDrawer />
           <UserArea />
         </div>
